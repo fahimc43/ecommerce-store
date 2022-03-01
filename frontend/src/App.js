6ece6c774d2d7050f5e7884e29154ddf5a7e5521
Fahim Chowdhury <fahimc43@gmail.com>
2022-03-01 08:32:12 +0600

    user component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
+import Login from "./components/user/Login";
+import Register from "./components/user/Register";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home} />
           <Route path="/product/:id" component={ProductDetails} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </div>
         <Footer />
       </div>
---
+++ frontend/src/actions/userAction.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FALL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FALL,
+  CLEAR_ERRORS,
+} from "../constants/userConstants";
+
+//   Login
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/v1/login",
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FALL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// User Register
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const { data } = await axios.post("/api/v1/register", userData, config);
+
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FALL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
